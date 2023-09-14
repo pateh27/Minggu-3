50b539f4c24ea9d5ca79f437cfc50fcd511c4cf1
pateh27 <fathzfl@gmail.com>
2023-09-14 21:33:08 +0700

    heheh

---
+++ ContohTipeData19.java
@@ -0,0 +1,20 @@
+public class ContohTipeData19 {
+    public static void main(String[] args) {
+        char golonganDarah = 'O';
+        byte jarak = (byte) 130;
+        short jumlahPendudukDalamSatuDusun = 1025;
+        float suhu = 60.50F;
+        double berat =0.5467812345;
+        long saldo = 1500000000;
+        int angkaDesimal = 0x10;
+
+        System.out.println("Golongan darah                      : " + (byte) golonganDarah);
+        System.out.println("jarak                               : " + jarak);
+        System.out.println("Jumlah pneududk dalam satu dusun    : " + jumlahPendudukDalamSatuDusun);
+        System.out.println("Suhu                                : " + suhu);
+        System.out.println("Berat                               : " + berat);    
+        System.out.println("Saldo                               : " + saldo);
+        System.out.println("Angka desimal                       : " + angkaDesimal);
+    }
+}
+    
